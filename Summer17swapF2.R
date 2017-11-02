a1fffaeadb1e8b5e3fc9c25a09cf31204d7e6837
Erik Parker <erikpark@indiana.edu>
2017-11-01 21:56:43 -0400

    11.1.17

--- Summer17swapF2.R
+++ Summer17swapF2.R
@@ -2,6 +2,7 @@ library(ggpubr)
 library(gridExtra)
 library(tidyr)
 library(alr4)
+library(GGally)
 
 f2.orig <- read.csv("Microbiome swap F2's - Sheet1.csv")
 
@@ -9,9 +10,19 @@ f2.fixed <- separate(data = f2.orig, col = Mother.Individual, into = c("mother",
 
 f2.fixed <- f2.fixed[-(35:40),]
 
+f2.fixed$Maternal.size <- as.factor(f2.fixed$Maternal.size)
+
+ggpairs(data = f2.fixed, columns = c(5,8))
+
 ggplot(f2.fixed, aes(x = mother, y = Time.to.adulthood, fill = Treatment.ID)) + geom_boxplot()
 
+ggplot(f2.fixed, aes(x = mother, y = Time.to.adulthood, fill = Treatment.ID)) + geom_boxplot() + facet_wrap(~Sex)
+
 ggplot(f2.fixed, aes(x = mother, y = Adult.size, fill = Treatment.ID)) + geom_boxplot()
+
+ggplot(f2.fixed, aes(x = Treatment.ID, y = Adult.size, fill = Sex)) + geom_boxplot()
+
+ggplot(f2.fixed, aes(x = mother, y = Adult.size, fill = Treatment.ID)) + geom_boxplot() + facet_wrap(~Sex)
 
 scatterplot(Adult.size ~  | Treatment.ID, data = f2.fixed)
 
@@ -22,6 +33,10 @@ mfull.time <- lm(Time.to.adulthood ~ Treatment.ID + Sex + BB.weight*Maternal.siz
 summary(mfull.time)
 
 Anova(mfull.time)
+
+mred.time <- lm(Time.to.adulthood ~ Treatment.ID + Maternal.size, data = f2.fixed)
+summary(mred.time)
+
 
 mfull.size <- lm(Adult.size ~ Treatment.ID + Sex + BB.weight*Maternal.size, data = f2.fixed)
 summary(mfull.size)
@@ -39,3 +54,10 @@ mmid.size <- lm(Adult.size ~ Treatment.ID + Sex + BB.weight:Maternal.size, data 
 summary(mmid.size)
 
 anova(mred.size,mmid.size)
+
+anova(mmid.size, mfull.size)
+
+mtest.size <- lm(Adult.size ~ Treatment.ID*Sex + BB.weight:Maternal.size, data = f2.fixed)
+summary(mtest.size)
+
+Anova(mtest.size)
