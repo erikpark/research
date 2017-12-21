6d1bd568ed71ab40f2c42a3d0608a2995f114a69
Erik Parker <erikpark@indiana.edu>
2017-12-21 14:43:39 -0500

    12.21.17

--- Summer17swapF2.R
+++ Summer17swapF2.R
@@ -11,6 +11,8 @@ f2.fixed <- separate(data = f2.orig, col = Mother.Individual, into = c("mother",
 
 f2.fixed <- f2.fixed[-(35:40),]
 
+f2.fixed <- f2.fixed[complete.cases(f2.fixed[,1]),]
+
 f2.fixed$Maternal.size <- as.factor(f2.fixed$Maternal.size)
 
 ggpairs(data = f2.orig, columns = c(4,11))
@@ -18,6 +20,34 @@ ggpairs(data = f2.orig, columns = c(4,11))
 ggpairs(data = f2.orig, columns = c(7,11))
 
 ggpairs(data = f2.fixed, columns = c(5,8))
+
+# Looking at mortality
+dead <- f2.fixed[f2.fixed$Dead. == "y",]
+alive <- f2.fixed[f2.fixed$Dead. == "",]
+
+gg <- f2.fixed[f2.fixed$Treatment.ID == "gg",]
+
+gs <- f2.fixed[f2.fixed$Treatment.ID == "gs",]
+
+gg.survival <- (nrow(gg) - (sum(gg$Dead. == "y"))) / nrow(gg)
+
+gs.survival <- (nrow(gs) - (sum(gs$Dead. == "y"))) / nrow(gs)
+
+
+ggvgs.surv <- prop.test(x = c((nrow(gg) - (sum(gg$Dead. == "y"))), (nrow(gs) - (sum(gs$Dead. == "y")))), n = c(nrow(gg), nrow(gs)))
+
+ggvgs.surv
+
+survivals <- c(gg.survival,gs.survival)
+survivals <- as.data.frame(survivals)
+survivals$treatment <- c("gg", "gs")
+
+
+ggplot(survivals, aes(x = treatment, y = survivals)) + geom_point() + expand_limits(y = 0)
+
+
+# So, no difference in survival rate between the treatment F2's
+
 
 
 ggplot(f2.fixed, aes(x = mother, y = Time.to.adulthood, fill = Treatment.ID)) + geom_boxplot()
@@ -69,6 +99,12 @@ Anova(mfull.size)
 mred.size <- lm(Adult.size ~ Treatment.ID + Sex, data = f2.fixed)
 summary(mred.size)
 
+mbb.size <- lm(Adult.size ~ Treatment.ID + Sex + BB.weight, data = f2.fixed)
+summary(mbb.size)
+
+mmom.size <- lm(Adult.size ~ Treatment.ID + Sex + Maternal.size, data = f2.fixed)
+summary(mmom.size)
+
 Anova(mred.size)
 
 anova(mred.size, mfull.size)
@@ -82,11 +118,15 @@ plot(Effect(c("Treatment.ID","Sex","BB.weight"),mmid.size))
 
 plot(Effect(c("Treatment.ID","Sex","Maternal.size"),mmid.size))
 
+plot(Effect(c("Treatment.ID"),mfull.size))
+
+plot(Effect(c("Treatment.ID"),mred.size))
+
 anova(mred.size,mmid.size)
 
 anova(mmid.size, mfull.size)
 
-mtest.size <- lm(Adult.size^2 ~ Treatment.ID + Sex + log(BB.weight):Maternal.size, data = f2.fixed)
+mtest.size <- lm(Adult.size ~ Treatment.ID + Sex + BB.weight:(Maternal.size)^2, data = f2.fixed)
 summary(mtest.size)
 
 #Testing variance
@@ -111,13 +151,8 @@ coeftest(mmid.size, vcov=hccm)
 # So, looks like all regressors except for treatment are well explained by constant variance, but treatment is very close to non-significant, and is a factor (so can't use it as the weight)
 # So it might be okay just the assume all the regressors have constant variance.
 
-e=resid(mmid.size)
-op=par(mfrow=c(2,2)) 
-with(f2.fixed,plot(fitted(mmid.size),e))
-with(f2.fixed,plot(Sex,e))
-with(f2.fixed,plot(Treatment.ID,e))
-with(f2.fixed,plot(BB.weight,e))
-par(op)
+
+summary(powerTransform(cbind(BB.weight,Maternal.size) ~ Treatment.ID, f2.fixed))
 
 
 vif(mmid.size)
@@ -125,3 +160,33 @@ vif(mfull.size)
 
 
 # So, best model seems to include treatment, sex and interaction between brood ball weight and maternal size (mmid.size)
+
+
+influenceIndexPlot(mfull.size, id.n = 3)
+# Should look at removing observations 79 and 98 at least, 79 because it has a really low bonferroni value, and 98 for a low bonferroni and high influence (Di)
+outlierTest(mfull.size)
+
+mfull.size.removed <- update(mfull.size, subset=-c(79,98))
+
+compareCoefs(mfull.size,mfull.size.removed)
+# So, removing the points reduces the estimated value for treatment very slightly.  Not a large change.
+
+Anova(mfull.size.removed)
+
+residualPlots(mfull.size)
+
+residualPlots(mfull.size.removed)
+# Also slightly improves the residuals.
+# But still looks like BB weight needs a transformation
+
+bc1 = powerTransform(cbind(BB.weight) ~ Maternal.size, f2.fixed)
+summary(bc1)
+# Maybe the sqrt (0.5) power?
+
+mfull.size.sqrt <- lm(Adult.size ~ Treatment.ID + Sex + sqrt(BB.weight)*Maternal.size, data = f2.fixed)
+summary(mfull.size.sqrt)
+Anova(mfull.size.sqrt)
+residualPlots(mfull.size.sqrt)
+outlierTest(mfull.size.sqrt)
+mfull.size.removed.sqrt <- update(mfull.size.sqrt, subset=-c(79,98))
+summary(mfull.size.removed.sqrt)
